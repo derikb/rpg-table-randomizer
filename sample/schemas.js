d0a17f7999a58f208f0d13b4696916f90f1abab3
Derik Badman <derik.badman@gmail.com>
2016-09-12 20:48:54 +0000

    more for sample schemas

--- sample/schemas.js
+++ sample/schemas.js
@@ -4,6 +4,22 @@
 
 module.exports = schemas = {};
 
+schemas.simple = {
+	key: 'simple',
+	title: 'Simple NPC',
+	author: 'Derik A Badman',
+	fields: [
+		{ key: 'name', type: 'string', source: 'name:random' },
+		{ key: 'occupation', type: 'string', source: 'table:medieval_occupations' },
+		{ key: 'appearance', type: 'array', source: 'table:onthenpc_appearance', count: 2 },
+		{ key: 'personality', type: 'array', source: 'table:onewordtraits', count: 2 },
+		{ key: 'goals', type: 'string', source: 'table:character_goals' },
+		{ key: 'reaction', type: 'number', source: 'roll:2d6' },
+		{ key: 'notes', type: 'text' },
+	]
+};
+
+
 schemas.colonial = {
 	key: 'colonial',
 	title: 'Colonial Campaign NPC',
