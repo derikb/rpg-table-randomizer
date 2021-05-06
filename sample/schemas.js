52c854416d9b9b7f59539efa73b1d957316ca7b5
Derik Badman <derik@springshare.com>
2021-05-06 09:02:16 -0400

    feat(npc): Add label to schema field
    Closes #28

--- sample/schemas.js
+++ sample/schemas.js
@@ -6,9 +6,9 @@ const simple = {
 	title: 'Simple NPC',
 	author: 'Derik A Badman',
 	fields: [
-		{ key: 'name', type: 'string', source: 'name:random' },
-		{ key: 'occupation', type: 'string', source: 'table:medieval_occupations' },
-		{ key: 'appearance', type: 'array', source: 'table:onthenpc_appearance', count: 2 },
+		{ key: 'name', type: 'string', source: 'name:random', label: 'Name' },
+		{ key: 'occupation', type: 'string', source: 'table:medieval_occupations', label: 'Job' },
+		{ key: 'appearance', type: 'array', source: 'table:onthenpc_appearance', count: 2, label: 'Phys. Appearance' },
 		{ key: 'personality', type: 'array', source: 'table:onewordtraits', count: 2 },
 		{ key: 'goals', type: 'string', source: 'table:character_goals' },
 		{ key: 'reaction', type: 'number', source: 'roll:2d6' },
