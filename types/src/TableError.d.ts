1c8df30b148b47c78c3d091065f1ef360925e6d9
Derik Badman <derik@derikbadman.com>
2024-06-22 10:25:35 -0400

    feat: remove typescript test

--- types/src/TableError.d.ts
+++
@@ -1,9 +0,0 @@
-export default TableError;
-/**
- * Custom error for handling known errors in the table roller.
- * @prop {String} key RandomTable key
- */
-declare class TableError extends Error {
-    constructor(message: any, key?: string);
-    key: string;
-}
